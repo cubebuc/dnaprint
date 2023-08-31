b0281bc030fc0f9dd3d5d07bca9c47c97abd0f12
Daniel Martinek <martid12@fit.cvut.cz>
2023-08-31 03:43:21 +0200

    Placement adjusted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ function Navigation()
     return (
         <div className="Navbar container-fluid text-center">
             <NavbarSection color='success' image={require('../resources/foto.webp')} text='foto' />
-            <NavbarSection color='primary' image={require('../resources/grafika.webp')} text='foto' />
-            <NavbarSection color='danger' image={require('../resources/redakce.webp')} text='foto' />
-            <NavbarSection color='warning' image={require('../resources/tisk.webp')} text='foto' />
-            <NavbarSection color='secondary' image={require('../resources/reklama.webp')} text='foto' />
+            <NavbarSection color='primary' image={require('../resources/grafika.webp')} text='grafika' />
+            <NavbarSection color='danger' image={require('../resources/redakce.webp')} text='redakce' />
+            <NavbarSection color='warning' image={require('../resources/tisk.webp')} text='tisk' />
+            <NavbarSection color='secondary' image={require('../resources/reklama.webp')} text='reklama' />
         </div>
     );
 }
