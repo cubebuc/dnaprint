9412045df5efdfb14653b8452e23c2e8f10fdd44
Daniel Martinek <martid12@fit.cvut.cz>
2023-08-31 03:43:21 +0200

    Placement adjusted

--- src/components/NavbarSection.js
+++ src/components/NavbarSection.js
@@ -5,7 +5,7 @@ function NavbarSection({ color, image, text })
     return (
         <div className="NavbarSection d-inline-flex justify-content-center">
             <img className={`${color} mx-2`} src={image} alt='' />
-            <div className="z-1 position-absolute" >
+            <div className="z-1 position-absolute d-flex justify-content-center align-items-center pt-2" >
                 <Title color={color} text={text} />
             </div>
         </div>
