1d2418f08dbcced47bb26401303b9a0d0a0603e7
Daniel Martinek <martid12@fit.cvut.cz>
2023-09-01 14:27:01 +0200

    Fix (remove) transition animation for HashLink

--- src/index.js
+++ src/index.js
@@ -15,12 +15,14 @@ import Footer from './components/Footer';
 function App()
 {
     const location = useLocation();
+    let pathname = location.pathname;
+    pathname = pathname[pathname.length - 1] === '/' ? pathname.slice(0, -1) : pathname;
 
     return (
         <div>
             <Header />
             <TransitionGroup component={null}>
-                <CSSTransition key={location.pathname} classNames='fade' timeout={400}>
+                <CSSTransition key={pathname} classNames='fade' timeout={400}>
                     <Routes location={location}>
                         <Route path='/dnaprint' element={<HomePage />} />
                         <Route path='/dnaprint/tiskoviny' element={<TiskovinyPage />} />
