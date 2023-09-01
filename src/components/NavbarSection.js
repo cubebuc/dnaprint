66f204cf50708b702c2871139a580d566f3b204f
Daniel Martinek <martid12@fit.cvut.cz>
2023-09-01 15:42:46 +0200

    HashLink improvements

--- src/components/NavbarSection.js
+++ src/components/NavbarSection.js
@@ -5,7 +5,7 @@ import Title from './Title';
 function NavbarSection({ color, text })
 {
     return (
-        <HashLink className='NavbarSection d-inline-flex justify-content-center text-decoration-none' smooth to={`./#${text}`}>
+        <HashLink className='NavbarSection d-inline-flex justify-content-center text-decoration-none' smooth to={`#${text}`}>
             <img className='mx-2' src={require(`../resources/${color}.jpg`)} width={180} height={334} alt='' />
             <div className='z-1 position-absolute d-flex justify-content-center align-items-center' >
                 <Title color={color} text={text} />
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,3 +1,5 @@
+import { HashLink } from 'react-router-hash-link';
+
 import Title from './Title';
 
 function Section({ color, title, subtitle, text })
@@ -12,6 +14,10 @@ function Section({ color, title, subtitle, text })
                     <Title color={color} text={title} scale={1.5} />
                     <h2 className='lead mb-4 fs-3'>{subtitle}</h2>
                     <p className='lead mt-2 text-light fs-5'>{text}</p>
+
+                    <HashLink className='mt-5 align-self-start' smooth to={`./#`}>
+                        <img src={require('../resources/arrow.png')} width={85} height={85} alt='' style={{ filter: 'invert(.3)' }} />
+                    </HashLink>
                 </div>
             </div>
         </div>
