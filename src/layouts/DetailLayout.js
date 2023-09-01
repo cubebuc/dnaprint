e86e329bee32a9435c8d5365751de92efae215c3
Daniel Martinek <martid12@fit.cvut.cz>
2023-09-01 21:28:52 +0200

    Responsive DetailPage

--- src/layouts/DetailLayout.js
+++ src/layouts/DetailLayout.js
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import MainLayout from './MainLayout';
 
 function DetailLayout({ children })
 {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() =>
+    {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    });
+
+    const isDesktop = width >= 1200;
+
     return (
         <MainLayout>
-            <div className='container ps-5 my-5'>
+            <div className='DetailLayout container ps-5 my-5'>
                 <div className='row'>
-                    <div className='col-3'>
-                        <img className='warning' src={require('../resources/warning.jpg')} width={252} height={682} alt='' />
-                    </div>
-                    <div className='col-6 mt-5'>
+                    {isDesktop &&
+                        <div className='col-3'>
+                            <img className='warning' src={require('../resources/warning.jpg')} width={252} height={682} alt='' />
+                        </div>
+                    }
+                    <div className='col-xl-6 mt-5'>
                         {children}
                     </div>
                 </div>
